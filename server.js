f876abf8cdb81bafd8c8f5094af1b313e5b1e95e
Luke Hendrick <lukehendrick41@gmail.com>
2017-08-04 13:01:10 -0500

    Added HTTPS redirect and modified Readme.md

--- server.js
+++ server.js
@@ -8,13 +8,14 @@ app.use(compression());
 app.use(express.static(__dirname + '/dist'));
 
 app.get('/', (req, res) => {
-    res.sendFile('index.html');
+    if (req.headers['x-forwarded-proto'] === 'https') {
+            res.sendFile('index.html');
+    };
+    res.redirect('https://' + req.hostname + req.url)
 })
 
 app.get('/api/get/*', (req, res) => {
-    console.log(req);
     let source = req.query.source;
-    console.log(source);
     api.newsGet(source).then((data) => {
         res.send(data.articles);
     })
